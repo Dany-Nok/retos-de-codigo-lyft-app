2ed643ec31877c7238e1d16a3c95f43908ea427b
Dany-Nok <daniela_gallardo_m@hotmail.com>
2017-12-14 16:58:00 -0300

    En construccion

---
+++ js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+// FUNCION PARA SPLASH (O PRELOADER)
+  $(function(){
+    setTimeout(function() {
+        $('#splash').fadeOut(500);
+      }, 3000);
+    });
+
+});
+
+// FUNCION PARA IR AL SIGUIENTE MODAL
+
+/*  $('#sign-up').modal('hide');
+  $('#verify').modal('show'); */
+
+
+
+
+
+// FUNCION PARA GENERAR NUMERO ALEATORIO
+/* var myNumeroAleatorio = Math.floor(Math.random()*(999-000))+000 */
